2018f29a66b7b211ebddd2dc4cbfa3a4e7d65f69
lemon-lime-honey <bs0308@g.skku.edu>
2023-12-01 22:38:23 +0900

    Update todo

--- todo/src/main/java/com/springboot/todo/controller/TodoController.java
+++ todo/src/main/java/com/springboot/todo/controller/TodoController.java
@@ -2,22 +2,43 @@ package com.springboot.todo.controller;
 
 import java.util.List;
 
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.springboot.todo.service.TodoService;
 import com.springboot.todo.vo.ResultDto;
 import com.springboot.todo.vo.Todo;
+
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping("/todo")
+    @ResponseBody
     public List<Todo> getTodoList(){
-        List<Todo> list=null;
+        List<Todo> list = todoService.getTodoList();
         return list;
     }
 
-    public ResultDto addTodo(Todo todo) {
+    @PostMapping("/todo")
+    @ResponseBody
+    public ResultDto addTodo(HttpServletResponse response, @RequestBody Todo todo) {
+        Todo target = new Todo(todo.getContent());
+        todoService.addTodo(target);
+        response.setStatus(HttpServletResponse.SC_OK);
         return new ResultDto(200, "Success");
     }
 
-    public ResultDto deleteTodo(@PathVariable("id") Integer id) {
+    @DeleteMapping("/todo/{id}")
+    public ResultDto deleteTodo(HttpServletResponse response, @PathVariable("id") Integer id) {
+        todoService.deleteTodo(id);
+        response.setStatus(HttpServletResponse.SC_OK);
         return new ResultDto(200, "Success");
     }
-
 }
--- todo/src/main/java/com/springboot/todo/service/TodoService.java
+++ todo/src/main/java/com/springboot/todo/service/TodoService.java
@@ -2,18 +2,28 @@ package com.springboot.todo.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.springboot.todo.vo.Todo;
 
+@Service
 public class TodoService {
     private List<Todo> todoList;
 
+    public TodoService(List<Todo> todoList) {
+        this.todoList = todoList;
+    }
+
     public void addTodo(Todo todo) {
+        todo.setId(todoList.size());
+        todoList.add(todo);
     }
 
     public List<Todo> getTodoList(){
-        return null;
+        return todoList;
     }
 
     public void deleteTodo(int index) {
+        todoList.remove(index);
     }
 }
